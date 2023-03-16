428ee72beeabc27b5a2e988ec76932263fb075c0
Mike Tagg <mdtagg91@gmail.com>
2023-03-16 13:17:30 -0700

    styling adjustments

--- src/containers/AreaTitle/utils/getWeatherIcon.jsx
+++ src/containers/AreaTitle/utils/getWeatherIcon.jsx
@@ -6,7 +6,6 @@ import { ReactComponent as Snow } from '../../../assets/svg/snow.svg'
 import { ReactComponent as Thunder } from '../../../assets/svg/thunder.svg'
 
 function GetWeatherIcon(weatherIcon) {
-    console.log({weatherIcon})
     return (
     weatherIcon === 0 ? Sun :
     weatherIcon === 1 ? CloudSun :
@@ -20,8 +19,3 @@ function GetWeatherIcon(weatherIcon) {
 
 export default GetWeatherIcon
 
-// weatherIcon === 0 ? newIcon = '/src/assets/svg/sun.svg' :
-// weatherIcon === 1 ? newIcon = '/src/assets/svg/cloud-sun.svg' :
-// weatherIcon >= 3 && weatherIcon <= 48 ? newIcon = '/src/assets/svg/cloud':
-// weatherIcon >= 71 && weatherIcon <= 77 ? newIcon = '/src/assets/svg/snow.svg':
-// weatherIcon >= 95 && weatherIcon <= 99 ? newIcon = '/src/assets/svg/thunder.svg':
