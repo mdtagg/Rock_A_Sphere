c6099f217544d4be6a3999f216a38d7e9b1fec62
Mike Tagg <mdtagg91@gmail.com>
2023-03-20 16:01:44 -0700

    lowered height and width of area title panel and map view to accomadate areas with more rock types

--- src/containers/Table/TableHead.jsx
+++ src/containers/Table/TableHead.jsx
@@ -5,16 +5,16 @@ const TableHead = () => {
         <thead>
             <tr class='flex wide:text-xs sm:text-xs'>
                 <th class='flex-col border-r-2 border-black p-1 w-1/5 flex justify-center items-center'>
-                    <p>Past 7 Rain Total</p>
+                    Past 7 Rain Total
                 </th>
                 <th class='border-r-2 border-black w-1/5 p-1 flex justify-center items-center'>Past 3 Rain Total</th>
                 <th class='flex-col border-r-2 border-black w-1/5 p-1  flex justify-center items-center'>
                     Primary Rock Type
-                    <p class='sm:text-[.4rem] text-[.7rem]'>(click for more info)</p>
+                    <p class='flex justify-center items-center w-full sm:text-[.4rem] text-[.7rem]'>(click for more info)</p>
                 </th>
                 <th class='flex-col border-r-2 border-black w-1/5 p-1  flex justify-center items-center'>
                     Other Rock Types
-                    <p class='sm:text-[.4rem] text-[.7rem]'>(click for more info)</p>
+                    <p class='flex justify-center items-center w-full sm:text-[.4rem] text-[.7rem] '>(click for more info)</p>
                 </th>
                 <th class=' w-1/5  flex justify-center items-center'>Days Of Fragile Rock Left</th>
             </tr>
