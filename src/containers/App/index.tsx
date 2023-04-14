037bf1ee2ad50e6a07efdb979a89a64212e265e0
Mike Tagg <mdtagg91@gmail.com>
2023-04-14 15:01:03 -0700

    bug fix: main background image not loading
    issue fixed by adding ts and tsx files to tailwind content property
    in config file

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -98,7 +98,7 @@ const App = () => {
     },[location])
 
     return (
-        <main className={`bg-[url('./assets/images/redRock.jpg')] bg-cover bg-center h-screen w-screen flex flex-col pt-10 justify-between sm:p-0 wide:p-0 wide:justify-center `}>
+        <main className="bg-[url('./assets/images/redRock.jpg')] bg-cover bg-center h-screen w-screen flex flex-col pt-10 justify-between sm:p-0 wide:p-0 wide:justify-center">
             <CurrentInfoContext.Provider value={currentInfoContextValues}>
 
                 <CurrentInfoDisplay />
