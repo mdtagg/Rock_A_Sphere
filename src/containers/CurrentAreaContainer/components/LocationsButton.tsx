c55ae52175a6f590b110012bb761a70f835cbb08
Mike Tagg <mdtagg91@gmail.com>
2024-01-29 14:32:43 -0800

    formatting change to colum

--- src/containers/CurrentAreaContainer/components/LocationsButton.tsx
+++ src/containers/CurrentAreaContainer/components/LocationsButton.tsx
@@ -12,31 +12,72 @@ const LocationsButton = () => {
     const { location } = useContext(CurrentInfoContext)!
     const { setEarthView } = useContext(EarthViewContext)!
 
-    const rotate = 
-    dropdown ? 'animate-spinUp transform rotate-[540deg]' : 'animate-spinDown'
+    const rotate = dropdown ? 'animate-spinUp transform rotate-[540deg]' : 'animate-spinDown'
 
-    function handleViewChange(setEarthView:ReactSetter<boolean>) {
-       setEarthView(true)
-    }
     return (
-        <div className='flex w-full gap-2 justify-center items-center xl:w-auto'>
+        <div 
+            className='
+                flex 
+                w-full 
+                gap-2 
+                justify-center 
+                items-center 
+                xl:w-auto
+                '
+            >
             <Earth
-                className='flex rounded-[100%] justify-center items-center h-6 w-7 cursor-pointer hover:bg-slate-500/50 sm:w-5 sm:h-5 wide:h-5 wide:w-5'
-                onClick={() => handleViewChange(setEarthView)}
+                className='
+                    flex 
+                    rounded-[100%] 
+                    justify-center 
+                    items-center 
+                    h-6 
+                    w-7 
+                    cursor-pointer 
+                    hover:bg-slate-500/50 
+                    sm:w-5 
+                    sm:h-5 
+                    wide:h-5 
+                    wide:w-5
+                '
+                onClick={() => setEarthView(true)}
             />
             <button 
-                className='flex rounded gap-2 justify-center items-center sm:w-3/4 wide:w-3/4 xl:w-full' 
-                onClick={() => {
-                    setDropdown(!dropdown)
-                }}
+                className='
+                    flex 
+                    rounded 
+                    gap-2 
+                    justify-center 
+                    items-center 
+                    sm:w-3/4 
+                    wide:w-3/4 
+                    xl:w-full
+                    ' 
+                onClick={() => setDropdown(!dropdown)}
             >
                 <p
-                    className='text-black font-bold text-2xl sm:text-xl sm:truncate wide:truncate wide:text-base xl:w-full'
+                    className='
+                        text-black 
+                        font-bold 
+                        text-2xl 
+                        sm:text-xl 
+                        sm:truncate 
+                        wide:truncate 
+                        wide:text-base 
+                        xl:w-full
+                        '
                 >
                     {location.title}
                 </p>
                 <DownCaret
-                    className={`flex justify-center items-center h-4 w-4 ${rotate}`}
+                    className={`
+                        flex 
+                        justify-center 
+                        items-center 
+                        h-4 
+                        w-4 
+                        ${rotate}
+                    `}
                 />
             </button>
         </div>
