bc504cbdad8dbb231652e975690f61c6751be30d
Mike Tagg <mdtagg91@gmail.com>
2024-01-31 12:06:02 -0800

    context files deleted, consolidated into one file

--- src/containers/App/contexts/CurrentInfoContext.ts
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react"
-import { CurrentInfoContextType } from "../types/app"
-
-const CurrentInfoContext = createContext<null | CurrentInfoContextType>(null)
-
-export default CurrentInfoContext
--- src/containers/App/contexts/WeatherContext.ts
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-import { WeatherContextType } from "..";
-
-const WeatherContext = createContext<WeatherContextType | null>(null)
-
-export default WeatherContext
