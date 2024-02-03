5fe7b8586f0318f097e96203131e7d09a0573670
Mike Tagg <mdtagg91@gmail.com>
2024-02-02 17:20:00 -0800

    states lifted from table body component to table container
    done to make the table easier to understand and its components less
    reliant on context

--- src/containers/TableContainer/index.tsx
+++ src/containers/TableContainer/index.tsx
@@ -1,10 +1,23 @@
-import { useContext } from "react"
-import { TableBody } from "../TableBody";
+import RockDataService from "../../services/RockDataService";
+import { TotalRainType,RockDataType,TRockType } from "../App/types/app";
+import { parseRainData } from "./helpers/parseRainData";
+import { parseRockData } from "./helpers/parseRockData";
+import { useState,useEffect,useContext } from "react"
+import { Header } from "./components/Header";
+import { LocationContext } from "../App/contexts/FormContext";
 import { TableInfoContext } from "../App/contexts/FormContext";
+import { TextNode } from "./components/TextNode";
+import { AnchorNode } from "./components/AnchorNode";
+import { RockList } from "./components/RockList";
+import { ListWindow } from "./components/ListWindow";
 
 const TableContainer = () => {
 
     const { buttonTitle } = useContext(TableInfoContext)!
+    const { weatherData, location } = useContext(LocationContext)!
+    const [ totalRain, setTotalRain ] = useState<TotalRainType | undefined>(undefined)
+    const [ rockTypes ,setRockTypes ] = useState<Map<any,any> | undefined>(undefined)
+    const [ rockData, setRockData ] = useState<RockDataType | undefined>(undefined)
 
     const hide = 
     buttonTitle !== 'Wet Rock' ? 'hidden' : 'flex'
@@ -17,51 +30,87 @@ const TableContainer = () => {
         "Days of Fragile Rock Left"
     ]
 
-    const moreInfoText = () => {
-        return (
-            <p
-                className="sm:text-[.4rem] text-[.7rem]"
-            >
-                (click for more info)
-            </p>
-        )
-    }
+    const content = rockData ? [
+        <TextNode
+            text={`${totalRain?.pastSevenTotal}"`}
+            color={`${totalRain?.pastSevenColor}`}
+        >
+            <AnchorNode
+                text="Weather data from Open-Medio"
+                href="https://open-meteo.com/"
+            />
+        </TextNode>,
 
-    const Theader = (title:string) => {
+        <TextNode
+            text={`${totalRain?.pastThreeTotal}"`}
+            color={`${totalRain?.pastThreeColor}`}
+        />,
+        <>
+            <RockList
+                list={rockData!.primaryRockType}
+            />
+            <AnchorNode
+                text="Geological data from MacroStrat"
+                href={`https://macrostrat.org/map/loc/${location.coords.longitude}/${location.coords.latitude}#z=14`}
+            />
+        </>,
+        <ListWindow
+            reRenderData={rockData}
+            data={rockData.kindsOfRock}
+            pages={3}
+            MappingComponent={RockList}
+        />
+    ] : 
+    []
 
-        const moreInfo = 
-        title === "Primary Rock Type" || title === "Other Rock Types" ? 
-        moreInfoText() : 
-        null
+    useEffect(() => {
+        if(!weatherData) return
+        const parsedRainData = parseRainData(weatherData.dailyWeather)
+        setTotalRain(parsedRainData)
+    },[weatherData])
 
-        return (
-            <th
-                key={title}
-                scope="col"
-                className="border-black border-b-2 border-r-2 p-1 w-1/5"
-            >
-                {title}
-                {moreInfo}
-            </th>
-        )
-    }
+    useEffect(() => {
+        (async function() {
+            const rockTypes = await RockDataService.getRockTypes()
+            const rockTypeMap = new Map()
+            rockTypes.success.data.forEach((val:TRockType) => rockTypeMap.set(val.lith_id,val))
+            setRockTypes(rockTypeMap)
+        })()
+    },[])
+
+    useEffect(() => {
+        if(!rockTypes) return
+        (async function() {
+            const currentLocationData = await RockDataService.getRockData(
+                location.coords.latitude,
+                location.coords.longitude
+            )
+            const rockData = parseRockData(currentLocationData,rockTypes)
+            setRockData(rockData)
+        })()
+    },[location,rockTypes])
     
     return (
         <table className={`${hide} flex-col w-2/5 bg-slate-200/50 border-t-2 border-l-2 border-black ml-11 sm:w-full sm:m-0 wide:w-screen wide:m-0 animate-fadeIn`}>
             <thead>
                 <tr className='flex wide:text-xs sm:text-xs'>
-                    {headerTitles.map((title,idx) => Theader(title))}
+                    {headerTitles.map((title) => Header(title))}
                 </tr>
             </thead>
-            <TableBody />
+            {rockData && totalRain &&
+            <tbody>
+                <tr className='flex items-center h-32 sm:h-[100px] wide:h-[100px] wide:text-sm'>
+                    {content.map(node => {
+                        return (
+                            <td className='flex flex-col border-r-2 border-b-2 border-black items-center justify-end w-1/5 p-1 h-full sm:p-0 '>
+                                {node}
+                            </td>
+                        )
+                    })}
+                </tr>
+            </tbody>}
         </table>
     )
 }
 
 export { TableContainer }
-
-/*
-fixed cell height widths
-
-
-*/
