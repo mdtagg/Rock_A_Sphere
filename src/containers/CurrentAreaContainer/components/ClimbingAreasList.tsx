941c49739fad986cffa76223db88362bbd877100
Mike Tagg <mdtagg91@gmail.com>
2024-02-04 16:59:03 -0800

    climbing area list component added
    added as a list component to be used with the listWindow component

---
+++ src/containers/CurrentAreaContainer/components/ClimbingAreasList.tsx
@@ -0,0 +1,53 @@
+import { useContext } from "react"
+import { FormContext } from "../../App/contexts/FormContext"
+import { LocationContext } from "../../App/contexts/FormContext"
+import { ReactSetter, TClimbingAreas,TClimbingArea } from "../../App/types/app"
+import DropDownContext from "../contexts/DropDownContext"
+
+export const ClimbingAreasList = (props:{list:TClimbingAreas}) => {
+
+    const { setClimbingAreas, climbingAreas } = useContext(FormContext)!
+    const { setLocation } = useContext(LocationContext)!
+    const { setDropdown } = useContext(DropDownContext)!
+    const { list } = props
+
+    function handleAreaChange(area:TClimbingArea) {
+        setDropdown(false)
+        setLocation(area)
+    }
+
+    function handleDelete(e:React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+        const { id } = e.currentTarget.dataset
+        const filteredAreas = climbingAreas.filter(area => area.id !== id)
+        setClimbingAreas(filteredAreas)
+    }
+
+    return (
+        <div
+            className="grid grid-rows-3 px-6 grid-cols-2 gap-1 w-full"
+        >
+        {list.map(area => {
+            return (
+                <div className='flex w-full gap-2 items-center bg-white rounded border-2 border-black sm:text-[9px] sm:font-bold sm:w-[90px] wide:w-[95px] wide:text-base transition-[height] ease-in duration-300' >
+                    <button
+                        className='h-[25px] w-full flex justify-start truncate font-medium items-center hover:bg-slate-500/50 sm:h-[20px] wide:text-[11px] wide:h-[10px]' 
+                        onClick={() => handleAreaChange(area)} 
+                    >
+                        {area.title}
+                    </button>
+                    <button
+                        className='font-bold hover:bg-red-500/50 pr-1 wide:text-sm'
+                        data-id={area.id} 
+                        onClick={(e) => handleDelete(e)}
+                    >
+                        x
+                    </button>
+                </div>
+            )
+        })
+        }
+        </div>
+        
+    )
+}
+
