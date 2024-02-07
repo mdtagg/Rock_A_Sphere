6624d2a7b7065cf8ba11f06071f460cfa1800cee
Mike Tagg <mdtagg91@gmail.com>
2024-02-07 11:53:54 -0800

    simplified function now that weater data is parsed in higher level

--- src/containers/App/helpers/parseCurrentWeather.ts
+++ src/containers/App/helpers/parseCurrentWeather.ts
@@ -3,11 +3,8 @@ import { numObj } from "./parseWeatherData";
 
 
 function parseCurrentWeather(data:numObj) {
-    const dateOptions:Intl.DateTimeFormatOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    const currentTemp = data.temperature
-    const currentDate = Intl.DateTimeFormat(undefined, dateOptions).format(data.time * 1000)
-    const weatherCode = data.weathercode
-
+    
+    const { currentTemp,currentDate,weatherCode } = data
     return {currentTemp,currentDate,weatherCode}
 }
 
