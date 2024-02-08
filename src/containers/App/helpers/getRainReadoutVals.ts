2075c3eca8f7af39a9e3d0acf95862bcfbd2914e
Mike Tagg <mdtagg91@gmail.com>
2024-02-08 15:05:03 -0800

    bug fix: wet rock vals now correctly displaying values

--- src/containers/App/helpers/getRainReadoutVals.ts
+++ src/containers/App/helpers/getRainReadoutVals.ts
@@ -17,30 +17,29 @@ function populateData(data:TDataLayerVal,i:number) {
         newObj[key] = data[key][i] 
     }
     newObj["color"] = getRainBgColor(colorData as number)
-    console.log({newObj,data})
     return newObj
 }
 
-function dateToData(data:TDataLayerVal,start:number,end:number) {
+function dateToData(data:IDaily | IHourly,start:number,end:number) {
     let newData = []
     for(let i = start;i < end;i++) {
         let dataLayer = {} as IDataLayer
         dataLayer["time"] = data.time[i] as string
-        dataLayer["vals"] = populateData(data,i) as TDataLayerVal
+        dataLayer["vals"] = populateData(data,i) 
         newData.push(dataLayer)
     }
     return newData
 }
 
 export const getRainReadoutVals = (daily:IDaily,hourly:IHourly) => {
-   
+    console.log({daily,hourly})
     const currentHour = new Intl.DateTimeFormat(undefined,{hour:'numeric'}).format(new Date())
 
-    const wetRockVals = dateToData(daily,0,7)
-    wetRockVals[0].time = "Today"
+    const wetRockVals = dateToData(daily,1,8)
+    wetRockVals[wetRockVals.length - 1].time = "Today"
 
     const forecastVals = dateToData(daily,7,14)
-    forecastVals[forecastVals.length - 1].time = "Today"
+    forecastVals[0].time = "Today"
 
     const hourlyVals = dateToData(hourly,0,168)
 
